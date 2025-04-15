a7cf15f8685e40585da925b8a0ff0797a7ff4555
Mr.Igor <iv_mirsoft@ukr.net>
2025-04-15 21:50:05 +0300

    t3

---
+++ js/tsk-03.js
@@ -0,0 +1,39 @@
+//const fruits = ['apple', 'plum', 'pear', 'orange', 'banana', 'nanana', 'kukuha'];
+
+//const firstTwoEls = fruits.slice(0, 2);
+//const nonExtremeEls = fruits.slice(1, fruits.length - 1);
+//const lastThreeEls = fruits.slice(fruits.length - 3);
+
+//console.log(firstTwoEls, nonExtremeEls, lastThreeEls);
+
+
+//const oldClients = ["Mango", "Ajax", "Poly", "Kiwi"];
+//const newClients = ["Peach", "Houston"];
+////const allClients = [...oldClients, ...newClients];
+//const allClients = oldClients.concat(newClients);
+
+//console.log(allClients);
+
+//function getSlice(array, value) {
+//	return array.indexOf(value) >= 0 ? array.slice(0, array.indexOf(value) + 1) : [];
+//}
+
+//console.log(getSlice(["Mango", "Poly", "Ajax"], "Masngo"));
+
+//function createArrayOfNumbers(min, max) {
+//	const arr = [];
+
+//	for (let i = min; i <= max; i++) {
+//		arr.push(i);
+//	}
+
+//	return arr;
+//}
+
+//console.log(createArrayOfNumbers(20, 31));
+
+
+//function checkStorage(storage, item) {
+//	return storage.includes(item.toLowerCase()) ? `${item} is available to order!` : "Sorry! We are out of stock!";
+
+//}
