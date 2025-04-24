e7420df8a80815985ec5a011b1b6a98ca3e891ce
Mr.Igor <iv_mirsoft@ukr.net>
2025-04-24 13:26:03 +0300

    ia++

--- js/tsk-04.js
+++ js/tsk-04.js
@@ -57,3 +57,107 @@ let b = [2, 41];
 b.push(...a)
 
 console.log(b);
+
+
+//2025/04/24
+
+const words = ["apple", "banana", "apple", "orange", "banana", "apple"];
+// Результат:
+// { apple: 3, banana: 2, orange: 1 }
+
+function contWords(mWords) {
+	return mWords.reduce((a, v) => { a[v] = a[v] ? ++a[v] : 1; return a }, {})
+
+	let nObj = {};
+
+	for (const element of mWords) {
+		if (nObj[element]) {
+			nObj[element]++;
+		}
+		else {
+			nObj[element] = 1;
+		}
+	}
+
+	return nObj;
+}
+
+console.log(contWords(words));
+
+const products = [
+	{ name: "iPhone", category: "tech" },
+	{ name: "T-shirt", category: "clothes" },
+	{ name: "MacBook", category: "tech" },
+	{ name: "Jeans", category: "clothes" },
+];
+
+// Результат:
+// {
+//   tech: ["iPhone", "MacBook"],
+//   clothes: ["T-shirt", "Jeans"]
+// }
+
+function getBack(arrOfObj) {
+
+	return arrOfObj.reduce((acc, { category, name }) => {
+		acc[category] = acc[category] ?? [];
+		acc[category].push(name);
+		return acc;
+	}, {});
+
+}
+
+console.log(getBack(products));
+
+
+const grades = [
+	{ subject: "math", score: 90 },
+	{ subject: "english", score: 85 },
+	{ subject: "math", score: 70 },
+	{ subject: "english", score: 95 },
+];
+
+// Результат:
+// {
+//   math: 80,      // (90 + 70) / 2
+//   english: 90    // (85 + 95) / 2
+// }
+
+function getMidGrades(arrOfObj) {
+	//let scObj = {};
+	//let cObj = {};
+
+	//for (const element of arrOfObj) {
+	//	if (scObj[element.subject]) {
+	//		scObj[element.subject] += element.score;
+	//		cObj[element.subject]++;
+	//	} else {
+	//		scObj[element.subject] = element.score;
+	//		cObj[element.subject] = 1;
+	//	}
+	//}
+
+	//for (const [key, value] of Object.entries(scObj)) {
+	//	scObj[key] = value / cObj[key];
+	//}
+
+	//return scObj;
+
+	const result = arrOfObj.reduce((acc, { subject, score }) => {
+		if (!acc[subject]) {
+			acc[subject] = { total: 0, count: 0 };
+		}
+		acc[subject].total += score;
+		acc[subject].count += 1;
+		return acc;
+	}, {});
+
+	// Підрахунок середніх значень
+	for (const subject in result) {
+		result[subject] = result[subject].total / result[subject].count;
+	}
+
+	return result;
+}
+
+console.log(getMidGrades(grades));
